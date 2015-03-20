4e397f9b089c55eddd06b98b1eb8319ea61cdae0
skyline75489 <skyline75489@outlook.com>
2015-03-20 22:15:03 +0800

    update

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
 var nextdate = require('./lib/nextdate.js');
-var express = require('express')
-var app = express()
+var express = require('express');
+
+var app = express();
 
 app.set('view engine', 'jade');
 app.get('/', function (req, res) {
     res.render('index', { title: 'Hey', message: 'Hello there!'});
-})
+});
 
-app.listen(3000)
+app.listen(3000);
