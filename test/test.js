2e865b2e9a1920dee46fd6e02b80d3f6a7ee5478
skyline75489 <skyline75489@outlook.com>
2015-03-20 15:36:52 +0800

    update

--- test/test.js
+++ test/test.js
@@ -4,14 +4,26 @@ var assert = require('assert');
 describe('nextdate()', function() {
   it('should work fine with a random normal date', function(){
     var d = nextdate.nextdate(1999, 4, 5)
-    assert.equal(d.getFullYear(), 1999);
-    assert.equal(d.getMonth(), 4);
-    assert.equal(d.getDate(), 6);
+    assert.equal(d.year, 1999);
+    assert.equal(d.month, 4);
+    assert.equal(d.day, 6);
   })
   it('should know about leap year', function(){
     var d = nextdate.nextdate(2000, 2, 28)
-    assert.equal(d.getFullYear(), 2000);
-    assert.equal(d.getMonth(), 2);
-    assert.equal(d.getDate(), 29);
+    assert.equal(d.year, 2000);
+    assert.equal(d.month, 2);
+    assert.equal(d.day, 29);
+  })
+  it('should know about non-leap year', function(){
+    var d = nextdate.nextdate(1999, 2, 28)
+    assert.equal(d.year, 1999);
+    assert.equal(d.month, 3);
+    assert.equal(d.day, 1);
+  })
+  it('should work fine when crossing month', function(){
+    var d = nextdate.nextdate(1999, 6, 30)
+    assert.equal(d.year, 1999);
+    assert.equal(d.month, 7);
+    assert.equal(d.day, 1);
   })
 })
