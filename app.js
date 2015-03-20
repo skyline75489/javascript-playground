86b83f520fdda0e4a6596ffb87404009e028bc58
skyline75489 <skyline75489@outlook.com>
2015-03-20 10:51:43 +0800

    express and jade

--- app.js
+++ app.js
@@ -1,3 +1,10 @@
-var nextdate = require('nextdate.js');
+var nextdate = require('./lib/nextdate.js');
+var express = require('express')
+var app = express()
 
+app.set('view engine', 'jade');
+app.get('/', function (req, res) {
+    res.render('index', { title: 'Hey', message: 'Hello there!'});
+})
 
+app.listen(3000)
